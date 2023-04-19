a91239efa81f5cc6083b3aceaf24f4182f9b065f
sholabioku <bilushonline@gmail.com>
2023-04-19 09:33:48 +0100

    Add functionality for error and exception handling

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' });
 });
 
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
