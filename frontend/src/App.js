9e69cce3296424eb5974555725e07da2c13ce8ff
sholabioku <bilushonline@gmail.com>
2023-04-23 11:01:43 +0100

    Add Login,Register and Home pages routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
-  return <div className='App'>My App</div>;
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
