455da73315ef5101c626c0458328ca1fae5da536
sholabioku <bilushonline@gmail.com>
2023-04-23 12:11:10 +0100

    Add jsx for Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in to get support</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              value={email}
+              name='email'
+              placeholder='Enter your email'
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              value={password}
+              name='password'
+              placeholder='Enter your password'
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
