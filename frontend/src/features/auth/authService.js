3223e01df223336783533cdeb4356e89dbe7ee79
sholabioku <bilushonline@gmail.com>
2023-04-26 08:40:45 +0100

    Add a fix for logout

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -10,7 +10,7 @@ const register = async (userData) => {
   return response.data;
 };
 
-const logout = localStorage.removeItem('user');
+const logout = () => localStorage.removeItem('user');
 
 const authService = {
   register,
