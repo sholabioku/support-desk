39b11ff756b662330130996b62b3fc6fca8e64da
sholabioku <bilushonline@gmail.com>
2023-04-26 09:08:40 +0100

    Add functionality to login user

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
-const API_URL = '/api/users';
+const API_URL = '/api/users/';
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+const login = async (userData) => {
+  const response = await axios.post(API_URL + 'login', userData);
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
@@ -14,6 +23,7 @@ const logout = () => localStorage.removeItem('user');
 
 const authService = {
   register,
+  login,
   logout,
 };
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaSignInAlt } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { login } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
+import { login, reset } from '../features/auth/authSlice';
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -13,10 +15,22 @@ const Login = () => {
   const { email, password } = formData;
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const { user, isLoading, isSuccess, message } = useSelector(
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.auth
   );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+    dispatch(reset());
+  }, [dispatch, isError, isSuccess, message, navigate, user]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -34,6 +48,8 @@ const Login = () => {
     };
     dispatch(login(userData));
   };
+
+  if (isLoading) return <Spinner />;
 
   return (
     <>
