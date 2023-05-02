38b6c73db29bdce780c0c4e5598817e41d962e30
sholabioku <bilushonline@gmail.com>
2023-05-02 18:19:33 +0100

    Add functionality to update and delete ticket

--- backend/middleware/ticketController.js
+++ backend/middleware/ticketController.js
@@ -68,8 +68,66 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+// @desc Delete ticket
+// @route DELETE /api/tickets/:id
+// @access Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+// @desc Update ticket
+// @route PUT /api/tickets/:id
+// @access Private
+const updateTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
 module.exports = {
   getTickets,
   getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -5,11 +5,17 @@ const {
   getTickets,
   createTicket,
   getTicket,
+  deleteTicket,
+  updateTicket,
 } = require('../middleware/ticketController');
 
 const router = express.Router();
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
-router.route('/:id').get(protect, getTicket);
+router
+  .route('/:id')
+  .get(protect, getTicket)
+  .put(protect, updateTicket)
+  .delete(protect, deleteTicket);
 
 module.exports = router;
