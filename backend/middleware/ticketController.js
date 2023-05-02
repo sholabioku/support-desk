81979a2c533adbbe08e3b672347715f9f6315923
sholabioku <bilushonline@gmail.com>
2023-05-02 17:48:59 +0100

    Add functionality to fetch user's ticket

--- backend/middleware/ticketController.js
+++ backend/middleware/ticketController.js
@@ -17,6 +17,30 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+// @desc Get user ticket
+// @route GET /api/tickets/:id
+// @access Private
+const getTicket = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+  if (!user) {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not Authorized');
+  }
+
+  res.status(200).json(ticket);
+});
+
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
@@ -46,5 +70,6 @@ const createTicket = asyncHandler(async (req, res) => {
 
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
 const protect = require('../middleware/authMiddleware');
-const { getTickets, createTicket } = require('../middleware/ticketController');
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+} = require('../middleware/ticketController');
 
 const router = express.Router();
 
 router.route('/').get(protect, getTickets).post(protect, createTicket);
+router.route('/:id').get(protect, getTicket);
 
 module.exports = router;
