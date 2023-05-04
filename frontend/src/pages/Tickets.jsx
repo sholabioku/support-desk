0a6311ffca37a38d1ae7a09b3350ad77e7ec6d2a
sholabioku <bilushonline@gmail.com>
2023-05-04 18:50:28 +0100

    Add functionality for listing ticktes in UI

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTickets, reset } from '../features/tickets/ticketSlice';
 import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
+import TicketItem from '../components/TicketItem';
 
 const Tickets = () => {
   const { tickets, isLoading, isSuccess } = useSelector(
@@ -22,9 +24,21 @@ const Tickets = () => {
   if (isLoading) return <Spinner />;
 
   return (
-    <div>
+    <>
+      <BackButton url='/' />
       <h1>Tickets</h1>
-    </div>
+      <div className='tickets'>
+        <div className='ticket-headings'>
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div></div>
+        </div>
+        {tickets.map((ticket) => (
+          <TicketItem key={ticket._id} ticket={ticket} />
+        ))}
+      </div>
+    </>
   );
 };
 
