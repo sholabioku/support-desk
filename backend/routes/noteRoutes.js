c7381479163cb1437fabce956095d665c1a0aa45
sholabioku <bilushonline@gmail.com>
2023-05-08 10:18:05 +0100

    Add functionality for POST /api/tickets/:ticketId/notes to add ticket

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router({ mergeParams: true });
 
 const protect = require('../middleware/authMiddleware');
 const { getNotes } = require('../controllers/noteController');
+const { addNote } = require('../controllers/noteController');
 
-router.route('/').get(protect, getNotes);
+router.route('/').get(protect, getNotes).post(protect, addNote);
 
 module.exports = router;
